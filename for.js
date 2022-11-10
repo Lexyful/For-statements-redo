ce8c8137aded584f0be63c8a40422e8007f7f8c5
Alexys Jordan <alexyskjordan@gmail.com>
2022-11-09 20:50:25 -0700

    Add challenge answers

--- for.js
+++ for.js
@@ -4,16 +4,26 @@
 // Challenge 1
 // Use a for statement to log to the console all even numbers between 25 and 75.
 
+for (var i = 25; i < 75; i++) {
+  console.log('filter');
+  // the 'filter' array method is meant to  return a new array containing only the even numbers
 
 
 // Challenge 2
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
-var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+// var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+// for (i=0 i < weather_options.length; i++) {
+//   console.log(`it's been a beautiful ${weather_options[i]} day`)
+// } 
+// i'm having a hard time getting this code to run without errors
 
 
 // Challenge 3
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
-var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+// var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+// const array = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+// array.forEach(item => {if(item.length%3 == 1)  console.log(item)});
+// this method is meant to pick out strings that are 3 characters in length
 
 // 📹 Record yourself live-coding and talking through any ONE of the challenges. 
   // The recording doesn't have to be the first time you are solving the problem. 
